d760bd91694549b1d207b8745964eec89c19fd9d
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-03 14:56:52 +0900

    ProfileModal img is aligned

--- final-pjt-front/src/components/ProfileModal.vue
+++ final-pjt-front/src/components/ProfileModal.vue
@@ -1,30 +1,48 @@
 <template>
   <div :id="id" class="w3-modal" style="display: none">
     <div class="w3-modal-content w3-animate-top w3-card-4" style="width=5rem">
+      <!-- 헤더 -->
       <header class="w3-container w3-dark-grey"> 
         <span @click="modalClose(id)" id="myBtn"
         class="w3-button w3-display-topright">&times;</span>
         <h2>{{movie.title}}</h2>
       </header>
       <br>
+      <!-- 바디 -->
+
+
+
+
+
+
+      
+      
+
+      
+      
+      
       <div class="w3-container">
-        <div class="w3-justify w3-container">
-          <div class="w3-center">
+        <div class="w3-justify w3-container w3-row-padding">
+          <div class="w3-center w3-col l4 m6" style="padding-bottom: 1.5rem">
             <img :src="movieURL()" class="w3-image w3-right w3-hide-small w3-greyscale">
-          <!-- <home-movie-card 
-          :movie="firstActorRecomend" :like="firstActorRecomend.like" :watched="firstActorRecomend.watched"
-          ></home-movie-card> -->
           </div>
           <br>
-          <p><strong>popularity: {{movie.popularity}}</strong></p>
-          <p>{{movie.overview}}</p>
-          <!-- 좋아요, 봤어요 -->
-          <p v-if="movie['like']==='true'" class="w3-left"><button class="w3-button w3-white w3-border" style="width: 120px;" @click="[likeClick($event), likeAxios(movie['local_id'])]">✓ Liked</button></p>
-          <p v-if="movie['like']==='false'" class="w3-left"><button class="w3-button w3-white w3-border" style="width: 120px;" @click="[likeClick($event), likeAxios(movie['local_id'])]"><i class="fa fa-thumbs-up"></i> Like</button></p>
-          <p v-if="movie['watched']==='true'" class="w3-left"><button class="w3-button w3-white w3-border" style="width: 120px;" @click="[watchedClick($event), watchedAxios(movie['local_id'])]">✓ Watched</button></p>
-          <p v-if="movie['watched']==='false'" class="w3-left"><button class="w3-button w3-white w3-border" style="width: 120px;" @click="[watchedClick($event), watchedAxios(movie['local_id'])]"><i class="fa fa-video-camera"></i> Watch</button></p>
+          <div class="w3-col l8 m6">
+            <p><strong>popularity: {{movie.popularity}}</strong></p>
+            <p>{{movie.overview}}</p>
+            <!-- 좋아요, 봤어요 -->
+            <p v-if="movie['like']==='true'" class="w3-left"><button class="w3-button w3-white w3-border" style="width: 120px;" @click="[likeClick($event), likeAxios(movie['local_id'])]">✓ Liked</button></p>
+            <p v-if="movie['like']==='false'" class="w3-left"><button class="w3-button w3-white w3-border" style="width: 120px;" @click="[likeClick($event), likeAxios(movie['local_id'])]"><i class="fa fa-thumbs-up"></i> Like</button></p>
+            <p v-if="movie['watched']==='true'" class="w3-left"><button class="w3-button w3-white w3-border" style="width: 120px;" @click="[watchedClick($event), watchedAxios(movie['local_id'])]">✓ Watched</button></p>
+            <p v-if="movie['watched']==='false'" class="w3-left"><button class="w3-button w3-white w3-border" style="width: 120px;" @click="[watchedClick($event), watchedAxios(movie['local_id'])]"><i class="fa fa-video-camera"></i> Watch</button></p>
+          </div>
         </div>
       </div>
+
+
+
+
+      <!-- 푸터 -->
       <footer class="w3-container w3-dark-grey">
         <p>recommended by {{ name }}</p>
       </footer>
